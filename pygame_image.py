3f1c7f4e8b7000f586f950dc2183793a3a07bdb4
Taichi <c0a23030f3@edu.teu.ac.jp>
2024-04-16 17:44:47 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -21,14 +21,18 @@ def main():
             if event.type == pg.QUIT: return
         
         key_lst = pg.key.get_pressed() #練習8-3
+        print(key_lst)
+        kk_rct.move_ip((-1, 0)) #演習1
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0, -1))
         if key_lst[pg.K_DOWN]:
             kk_rct.move_ip((0, 1))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1, 0))
+            kk_rct.move_ip((2, 0))
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
+        if key_lst == False:
+            kk_rct.move_ip((-x, 0))
 
         x = tmr % 3200 #練習6
         screen.blit(bg_img, [-x, 0]) #練習6
